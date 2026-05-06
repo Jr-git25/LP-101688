e0f6c7b18cecbf76792dcdbe4a1202888ab8c3f3
Jr-git25 <j.paz@ba.estudante.senai.br>
2026-05-06 16:57:42 -0300

    Add files via upload

--- exercicio_2.py
+++ exercicio_2.py
@@ -1,15 +1,90 @@
 import os
-os.system('cls')
+from dataclasses import dataclass
 
-while True:
-    usuario = input('Insira seu usuario: ')
-    password = input('Insira sua senha: ')
-    senha = '1234'
-    login = 'Lucas'
+# Configuração da Dataclass
+@dataclass
+class Livro:
+    nome: str
+    autor: str
+    categoria: str
+    preco: float
 
-    if usuario == login and password == senha:
-        print('Acesso liberado')
-        break
+    def mostrar_dados_livro(self):
+        print(f'Nome: {self.nome}')
+        print(f'Autor: {self.autor}')
+        print(f'Categoria: {self.categoria}')
+        print(f'Valor: R$ {self.preco}')
+        print('-' * 20)
+
+# --- Funções de Funcionalidade ---
+
+def cadastrar_livros():
+    """Lê os dados do usuário e salva diretamente no arquivo CSV."""
+    print('\n= Cadastro de Livro =')
+    nome = input('Insira o nome do carro: ')
+    autor = input('Qual o : ')
+    categoria = input('Qual a categoria do livro: ')
+    
+    try:
+        preco = float(input('Qual o valor do livro: '))
+    except ValueError:
+        print("Preço inválido! Usando 0.0")
+        preco = 0.0
+
+    # Salvando no arquivo (modo 'a' para append/anexar)
+    with open('Catalogo_livros.csv', 'a', encoding='utf-8') as arquivo:
+        arquivo.write(f'{nome},{autor},{categoria},{preco}\n')
+    
+    print('✔ Livro salvo com sucesso!')
+
+def listar_livros():
+    """Lê o arquivo CSV e exibe os livros cadastrados."""
+    print('\n= Consultar Catálogo =')
+    
+    if not os.path.exists('Catalogo_livros.csv'):
+        print("O arquivo de catálogo ainda não existe. Cadastre um livro primeiro.")
+        return
+
+    lista_livro1 = []
+    with open('Catalogo_livros.csv', 'r', encoding='utf-8') as arquivo:
+        for linha in arquivo:
+            # strip() remove o \n e split(',') separa as colunas
+            dados = linha.strip().split(',')
+            if len(dados) == 4:
+                nome, autor, categoria, preco = dados
+                lista_livro1.append(Livro(nome, autor, categoria, float(preco)))
+
+    if not lista_livro1:
+        print("Nenhum livro encontrado.")
     else:
-        print('Acesso negado')
-        
+        for livro in lista_livro1:
+            livro.mostrar_dados_livro()
+
+def exibir_menu_carros():
+    """Apenas imprime as opções do menu."""
+    print("\n--- SISTEMA DE CADASTRO ---")
+    print("1. Cadastrar Carros")
+    print("2. Listar Carros")
+    print("0. Sair")
+    return input("Escolha uma opção: ")
+
+# --- Fluxo Principal ---
+
+def main():
+    while True:
+        opcao = exibir_menu_carros()
+
+        match opcao:
+            case "1":
+                cadastrar_livros()
+            case "2":
+                listar_livros()
+            case "0":
+                print("Saindo do sistema... Até logo!")
+                break
+            case _:
+                print(" Opção inválida! Tente novamente.")
+
+if __name__ == "__main__":
+    main()
+    
--- exercicio_3.py
+++ exercicio_3.py
@@ -1,11 +1,92 @@
 import os
-os.system('cls')
+from dataclasses import dataclass
 
-def numero():
-    for i in range(1):
-        numero = float(input('Insira o numero desejado: '))
-    if numero % 2 == 0:
-        print("Este numero e par")
+# # Configuração da Dataclass
+# @dataclass
+# class carro:
+#     ano: str
+#     marca: str
+#     modelo: str
+#     preco: float
+
+
+@dataclass
+class Modelo_carro:
+    ano: int
+    marca: str
+    modelo: str
+    
+    def mostrar_dados_livro(self):
+        print(f'Ano do carro: {self.ano}')
+        print(f'Marca: {self.marca}')
+        print(f'Modelo: {self.modelo}')
+        print('-' * 20)
+
+# --- Funções de Funcionalidade ---
+
+
+NOME_DO_ARQUIVO = 'vista_carro.csv'
+
+def cadastrar_carro():
+    """Lê os dados do usuário e salva diretamente no arquivo CSV."""
+    novo_carro = Modelo_carro(
+
+        ano= int(input('Insira o ano do carro: ')),
+        marca= input('Qual o marca do carro: '),
+        modelo= input('Qual a modelo do carro: ')
+        # print('✔ carro salvo com sucesso!')
+    )
+    with open(NOME_DO_ARQUIVO, 'a', encoding='utf-8') as arquivo:
+        arquivo.write(f'{novo_carro.ano},{novo_carro.marca},{novo_carro.modelo}\n')
+
+    return novo_carro
+
+def listar_carros():
+    """Lê o arquivo CSV e exibe os livros cadastrados."""
+    print('\n= Consultar Catálogo =')
+    
+    if not os.path.exists(NOME_DO_ARQUIVO):
+        print("O arquivo de catálogo ainda não existe. Cadastre um carro primeiro.")
+        return
+
+    lista_carros = []
+    with open(NOME_DO_ARQUIVO, 'r', encoding='utf-8') as arquivo:
+        for linha in arquivo:
+            # strip() remove o \n e split(',') separa as colunas
+            dados = linha.strip().split(',')
+            if len(dados) == 4:
+                ano, marca, modelo, preco = dados
+                lista_carros.append(carro(ano, marca, modelo))
+
+    if not lista_carros:
+        print("Nenhum carro encontrado.")
     else:
-        print('Este numero e impar')
-numero()
+        for carro in lista_carros:
+            carro.mostrar_dados_livro()
+
+def exibir_menu_carros():
+    """Apenas imprime as opções do menu."""
+    print("\n--- SISTEMA DE CADASTRO ---")
+    print("1. Cadastrar Carros")
+    print("2. Listar Carros")
+    print("0. Sair")
+    return input("Escolha uma opção: ")
+
+
+def main():
+    while True:
+        opcao = exibir_menu_carros()
+
+        match opcao:
+            case 1:
+                cadastrar_carro()
+            case 2:
+                listar_carros()
+            case 0:
+                print("Saindo do sistema... Até logo!")
+                break
+            case _:
+                print(" Opção inválida! Tente novamente.")
+
+if __name__ == "__main__":
+    main()
